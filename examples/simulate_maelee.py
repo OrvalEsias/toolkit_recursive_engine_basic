eab692d32d188a521f637c91c0220cd4ccb4269e
OrvalEsias <emerbe3@gmail.com>
2025-07-17 13:06:59 -0500

    Add files via upload

---
+++ examples/simulate_maelee.py
@@ -0,0 +1,7 @@
+# simulate_maelee.py
+# Simulates Maelee's symbolic arc
+
+from engine.toolkit_engine import simulate_character
+
+if __name__ == "__main__":
+    simulate_character("characters/maelee_recursive_simulation.json")
---
+++ examples/world_shift_test.py
@@ -0,0 +1,7 @@
+# world_shift_test.py
+# Simulates symbolic change to a world state
+
+from engine.toolkit_engine import load_world
+
+if __name__ == "__main__":
+    load_world("worlds/metropolis_verge_world.json")
