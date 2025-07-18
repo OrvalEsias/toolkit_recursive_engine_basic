47d9252db97eeb25f79b881049e5f121ccbbc251
OrvalEsias <emerbe3@gmail.com>
2025-07-18 08:44:23 -0500

    Add files via upload

---
+++ examples/recursive_time_demo.py
@@ -0,0 +1,7 @@
+# recursive_time_demo.py
+# Demonstration of recursive time-based symbolic events
+
+from toolkit_engine import simulate_character
+
+if __name__ == "__main__":
+    simulate_character("characters/maelee_recursive_simulation.json")
